77aed41ceb4dd90bfc4d1e6dd61dcf4fb71ba844
Alannjaf <117263878+Alannjaf@users.noreply.github.com>
2025-02-02 15:35:25 +0300

    Fix View page

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -5,7 +5,7 @@ import NotFound from "@/pages/NotFound";
 import QuotationsIndex from "@/pages/quotations/Index";
 import NewQuotation from "@/pages/quotations/New";
 import EditQuotation from "@/pages/quotations/Edit";
-import ViewQuotation from "@/pages/quotations/View";
+import ViewQuotation from "@/pages/quotations";
 import UsersIndex from "@/pages/users/Index";
 import SettingsIndex from "@/pages/settings/Index";
 
