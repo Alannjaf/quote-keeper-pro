54ef0bc4744384ce2cfc405b72a95b4e9930e366
Alannjaf <117263878+Alannjaf@users.noreply.github.com>
2025-02-04 00:09:53 +0300

    remove quer client

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,7 @@ import './index.css'
 
 
 createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  </QueryClientProvider>
 );
