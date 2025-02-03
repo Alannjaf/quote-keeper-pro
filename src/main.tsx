e21e12cbbb9c0710e43313fdb3f15aadba5eddc4
Alannjaf <117263878+Alannjaf@users.noreply.github.com>
2025-02-03 22:26:57 +0300

    fix refetching data everytime

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,8 @@ import './index.css'
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 0, // Reset stale time to 0 to ensure fresh data
-      gcTime: 0, // Disable garbage collection temporarily to debug
+      staleTime: 5 * 60 * 1000, // Reset stale time to 0 to ensure fresh data
+      gcTime: 10 * 60 * 1000, // Disable garbage collection temporarily to debug
       retry: false, // Disable retries for failed queries
       refetchOnWindowFocus: false, // Enable refetch on window focus
       refetchOnMount: false, // Enable refetch on component mount
